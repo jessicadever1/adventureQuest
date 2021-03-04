71ac725e05094cf191f3ce8611bce3cb9597172b
Jess Dever <jessicadever1@gmail.com>
2021-03-04 14:33:28 -0600

    updated adventurer name - aka Phase 2

--- Program.cs
+++ Program.cs
@@ -14,6 +14,9 @@ namespace Quest
             //   the text of the challenge
             //   a correct answer
             //   a number of awesome points to gain or lose depending on the success of the challenge
+            Console.WriteLine("Welcome to Adventure Quest! What is your name?");
+            string userName = Console.ReadLine();
+
             Challenge twoPlusTwo = new Challenge("2 + 2?", 4, 10);
             Challenge theAnswer = new Challenge(
                 "What's the answer to life, the universe and everything?", 42, 25);
@@ -25,11 +28,11 @@ namespace Quest
 
             Challenge favoriteBeatle = new Challenge(
                 @"Who's your favorite Beatle?
-    1) John
-    2) Paul
-    3) George
-    4) Ringo
-",
+                    1) John
+                    2) Paul
+                    3) George
+                    4) Ringo
+                ",
                 4, 20
             );
 
@@ -43,7 +46,7 @@ namespace Quest
             int maxAwesomeness = 100;
 
             // Make a new "Adventurer" object using the "Adventurer" class
-            Adventurer theAdventurer = new Adventurer("Jack");
+            Adventurer theAdventurer = new Adventurer(userName);
 
             // A list of challenges for the Adventurer to complete
             // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
